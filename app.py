8c8b3ab99bada95f2e0317f025fccfca6874e198
ShmuelRozental <rozentalshmuel@gmail.com>
2024-09-19 11:13:35 +0300

    set sqllite

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, render_template
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fantasy_nba.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+
+
 
 
 if __name__ == '__main__':
